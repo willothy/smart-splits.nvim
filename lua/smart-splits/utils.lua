ac036a370e1242e7e28a0965343f8a9c82381ad4
Will Hopkins <willothyh@gmail.com>
2023-10-11 04:06:00 -0700

    fix for new win split PR

--- lua/smart-splits/utils.lua
+++ lua/smart-splits/utils.lua
@@ -16,7 +16,7 @@ end
 function M.is_floating_window(win_id)
   win_id = win_id or 0
   local win_cfg = vim.api.nvim_win_get_config(win_id)
-  return win_cfg and (win_cfg.relative ~= '' or not win_cfg.relative)
+  return win_cfg and win_cfg.zindex
 end
 
 function M.are_we_wezterm()
